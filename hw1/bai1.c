295e6f67f3a832ba850e77ab68a475fddb4ecdca
Vuong Phan <phanvuong118@gmail.com>
2026-03-20 11:20:12 +0700

    Exercise 1 & code runner settings (compile to build/ and ignore it)

--- hw1/bai1.c
+++ hw1/bai1.c
@@ -1,25 +1,68 @@
+#include <arpa/inet.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 
+// TCP client
 int main() {
     char inp[50];
     char cmd[16];
-    char ip_inp[16];
-    int port;
+    char server_ip[32];
+    int server_port;
 
     printf("Nhap lenh: ");
     fgets(inp, sizeof(inp), stdin);
-    int n = sscanf(inp, "%s %s %d", cmd, ip_inp, &port); // so bien phan tich duoc
+    int n = sscanf(inp, "%s %s %d", cmd, server_ip,
+                   &server_port); // so bien phan tich duoc
 
-    printf("n=%d\n", n);
-    printf("cmd=%s\n", cmd);
-    printf("ip_inp=%s\n", ip_inp);
-    printf("port=%d\n", port);
+    // printf("n=%d\n", n);
+    // printf("cmd=%s\n", cmd);
+    // printf("ip_inp=%s\n", server_ip);
+    // printf("port=%d\n", server_port);
 
     // kiem tra lenh
-    if(cmd != "tcp_client" || n != 3) {
+    if (strcmp(cmd, "tcp_client") != 0 || n != 3) {
         printf("Nhap sai lenh\n");
         return 0;
     }
 
+    // tao socket
+    int sock;
+    char message[1024];
+    sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    // chuyen doi dia chi IP
+    struct in_addr server_ip_addr;
+    if (inet_pton(AF_INET, server_ip, &server_ip_addr) <= 0) {
+        perror("IP address is invalid");
+        exit(EXIT_FAILURE);
+    }
+
+    // khai bao dia chi server
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(server_port);
+    server_addr.sin_addr = server_ip_addr;
+
+    // ket noi server
+    if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) <
+        0) {
+        perror("connect() failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // gui du lieu tu ban phim
+    printf("Nhap du lieu: ");
+    fgets(message, sizeof(message), stdin);
+    if (send(sock, message, strlen(message), 0) < 0) {
+        perror("send() failed");
+        exit(EXIT_FAILURE);
+    }
+
+    close(sock);
     return 0;
 }
